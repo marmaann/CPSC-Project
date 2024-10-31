873757acc389c02363584a6d099845dab45b5e98
Armaan Mehta <marmaan@vt.edu>
2024-10-31 12:11:02 -0400

    Add gunicorn to requirements.txt

--- CPSC/gunicorn-cfg.py
+++ CPSC/gunicorn-cfg.py
@@ -3,9 +3,11 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-bind = '0.0.0.0:5005'
-workers = 1
+
+bind = "0.0.0.0:8000"
+workers = 3
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
+
